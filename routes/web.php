f1c29e69539a3c3f38604109e5d09c3871174003
Franccesco <franccesco_@hotmail.com>
2022-05-21 11:00:07 -0300

    Seção 6: Super Gestão - Implementando as rotas login, clientes, fornecedores e produtos

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'PrincipalController@principal');
-
+Route::get('/contato', 'ContatoController@contato');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 Route::get('/login', function () { return 'Login'; });
@@ -22,8 +22,6 @@ Route::get('/clientes', function () { return 'clientes'; });
 Route::get('/fornecedores', function () { return 'Fornecedores'; });
 Route::get('/produtos', function () { return 'Produtos'; });
 // Nome, categoria, assunto mensagem
-
-
 
 // Route::get(
 //     '/contato/{nome}/{categoria_id}',
