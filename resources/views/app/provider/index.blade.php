7f7b23c2a9f1963c46207c85196f7bab4b3cfc83
Franccesco <franccesco_@hotmail.com>
2022-05-22 16:00:45 -0300

    Seção 7: @for

--- resources/views/app/provider/index.blade.php
+++ resources/views/app/provider/index.blade.php
@@ -1,24 +1,15 @@
 <h3>Provider</h3>
 
 @isset($providers)
-    Provider: {{ $providers[2]['name'] }}
-    <br />
-    Status: {{ $providers[2]['status'] }}
-    <br />
-    CNPJ: {{ $providers[2]['cnpj'] ?? 'Data was not fullfilled' }}
-    <br />
-    Telephone: {{ $providers[2]['ddd'] ?? '' }} {{ $providers[2]['tel'] ?? '' }}
-    @switch($providers[2]['ddd'])
-        @case('11')
-            São Paulo - SP
-            @break
-        @case('32')
-            Juiz de Fora - MG
-            @break
-        @case('85')
-            Fortaleza - CE
-            @break
-        @default
-            State not identified
-    @endswitch
+    @for ($i = 0; isset($providers[$i]); $i++)
+        Provider: {{ $providers[$i]['name'] }}
+        <br />
+        Status: {{ $providers[$i]['status'] }}
+        <br />
+        CNPJ: {{ $providers[$i]['cnpj'] ?? 'Data was not fullfilled' }}
+        <br />
+        Telephone: {{ $providers[$i]['ddd'] ?? '' }} {{ $providers[$i]['tel'] ?? '' }}
+
+        <hr />
+    @endfor
 @endisset
