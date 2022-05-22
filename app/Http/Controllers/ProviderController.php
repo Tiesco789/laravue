dd2859c120cad2b1cf7189eff7f572040c5e0f08
Franccesco <franccesco_@hotmail.com>
2022-05-22 15:56:22 -0300

    Seção 7: @switch/case

--- app/Http/Controllers/ProviderController.php
+++ app/Http/Controllers/ProviderController.php
@@ -8,8 +8,27 @@ class ProviderController extends Controller
 {
     public function provider() {
         $providers = [
-            0 => ['name' => 'provider 1', 'status' => 'N', 'cnpj' => '0'],
-            1 => ['name' => 'provider 2', 'status' => 'S', 'cnpj' => null]
+            0 => [
+                'name' => 'provider 1',
+                'status' => 'N',
+                'cnpj' => '0',
+                'ddd' => '0', // sao paulo (sp)
+                'tel' => '0000-0000'
+            ],
+            1 => [
+                'name' => 'provider 2',
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '85', // fortaleza (ce)
+                'tel' => '0000-0000'
+            ],
+            2 => [
+                'name' => 'provider 3',
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '32', // juiz de fora (mg)
+                'tel' => '0000-0000'
+            ],
         ];
 
         return view('app.provider.index', compact('providers'));
--- resources/views/app/provider/index.blade.php
+++ resources/views/app/provider/index.blade.php
@@ -1,9 +1,24 @@
 <h3>Provider</h3>
 
 @isset($providers)
-    Provider: {{ $providers[0]['name'] }}
+    Provider: {{ $providers[2]['name'] }}
     <br />
-    Status: {{ $providers[0]['status'] }}
+    Status: {{ $providers[2]['status'] }}
     <br />
-    CNPJ: {{ $providers[1]['cnpj'] ?? 'Data was not fullfilled' }}
+    CNPJ: {{ $providers[2]['cnpj'] ?? 'Data was not fullfilled' }}
+    <br />
+    Telephone: {{ $providers[2]['ddd'] ?? '' }} {{ $providers[2]['tel'] ?? '' }}
+    @switch($providers[2]['ddd'])
+        @case('11')
+            São Paulo - SP
+            @break
+        @case('32')
+            Juiz de Fora - MG
+            @break
+        @case('85')
+            Fortaleza - CE
+            @break
+        @default
+            State not identified
+    @endswitch
 @endisset
