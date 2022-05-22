691c59e46b8e18e9ea7770f06bb4c488768417fa
Franccesco <franccesco_@hotmail.com>
2022-05-22 14:07:32 -0300

    Seção 7: Operador condicional ternário

--- app/Http/Controllers/ProviderController.php
+++ app/Http/Controllers/ProviderController.php
@@ -11,6 +11,9 @@ class ProviderController extends Controller
             0 => ['name' => 'provider 1', 'status' => 'N', 'cnpj' => ''],
             1 => ['name' => 'provider 2', 'status' => 'S']
         ];
+
+        $msg = isset($providers[0]['cnpj']) ? 'CNPJ given' : 'CNPJ is not given';
+        echo $msg;
         return view('app.provider.index', compact('providers'));
 
     }
