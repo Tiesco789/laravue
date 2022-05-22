eae3e296833c60b98860de3164babb17273b0576
Franccesco <franccesco_@hotmail.com>
2022-05-22 13:44:48 -0300

    Seção 7: Blade - @isset

--- app/Http/Controllers/ProviderController.php
+++ app/Http/Controllers/ProviderController.php
@@ -8,7 +8,8 @@ class ProviderController extends Controller
 {
     public function provider() {
         $providers = [
-            0 => ['name' => 'provider 1', 'status' => 'N']
+            0 => ['name' => 'provider 1', 'status' => 'N', 'cnpj' => '00.000.000/000-00'],
+            1 => ['name' => 'provider 2', 'status' => 'S']
         ];
         return view('app.provider.index', compact('providers'));
 
--- resources/views/app/provider/index.blade.php
+++ resources/views/app/provider/index.blade.php
@@ -1,15 +1,11 @@
 <h3>Provider</h3>
 
-Provider: {{ $providers[0]['name'] }}
-<br />
-Status: {{ $providers[0]['status'] }}
-<br />
-
-@if(!($providers[0]['status'] == 'S'))
-    Fornecedor inativo
-@endif
-<br />
-
-@unless ($providers[0]['status'] == 'S')
-    Fornecedor inativo
-@endunless
+@isset($providers)
+    Provider: {{ $providers[0]['name'] }}
+    <br />
+    Status: {{ $providers[0]['status'] }}
+    <br />
+    @isset($providers[0]['cnpj'])
+        CNPJ: {{ $providers[0]['cnpj'] }}
+    @endisset
+@endisset
