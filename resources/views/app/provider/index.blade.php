be0e31fda92538bdd8ed2cd8cf2007952d61a427
Franccesco <franccesco_@hotmail.com>
2022-05-22 16:37:41 -0300

    Seção 7: Blade - Variável loop

--- resources/views/app/provider/index.blade.php
+++ resources/views/app/provider/index.blade.php
@@ -2,14 +2,25 @@
 
 @isset($providers)
     @forelse ($providers as $index => $provider)
+        Actual Iteration: {{ $loop->iteration }}
 
-        Provider: @{{ $provider['name'] }}
+        Provider: {{ $provider['name'] }}
         <br />
-        Status: @{{ $provider['status'] }}
+        Status: {{ $provider['status'] }}
         <br />
-        CNPJ: @{{ $provider['cnpj'] ?? 'Data was not fullfilled' }}
+        CNPJ: {{ $provider['cnpj'] ?? 'Data was not fullfilled' }}
         <br />
-        Telephone: @{{ $provider['ddd'] ?? '' }} @{{ $provider['tel'] ?? '' }}
+        Telephone: {{ $provider['ddd'] ?? '' }} {{ $provider['tel'] ?? '' }}
+        <br />
+        @if($loop->first)
+            First loop iteration
+            <br />
+            Total Registry: {{ $loop->count }}
+        @endif
+
+        @if($loop->last)
+            Last loop iteration
+        @endif
         <hr />
     @empty
         Provider does not exist
