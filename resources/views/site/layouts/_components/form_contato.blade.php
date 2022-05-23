a996995a2bf879ff39b04e6fe52f6d6e4e85c6cb
Franccesco <franccesco_@hotmail.com>
2022-05-23 12:45:20 -0300

    Seção 7: Blade - Enviando parâmetros para componentes

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -1,19 +1,22 @@
+{{ $slot }}
+{{ $classe }}
+
 <form action="{{ route('site.contact') }}" method="POST">
     @csrf
-    <input name="npme" type="text" placeholder="Nome" class="borda-preta">
+    <input name="npme" type="text" placeholder="Nome" class="{{ $classe }}">
     <br>
-    <input name="tel" type="text" placeholder="Telefone" class="borda-preta">
+    <input name="tel" type="text" placeholder="Telefone" class="{{ $classe }}">
     <br>
-    <input name="email" type="text" placeholder="E-mail" class="borda-preta">
+    <input name="email" type="text" placeholder="E-mail" class="{{ $classe }}">
     <br>
-    <select class="borda-preta" name="motivo_contato">
+    <select class="{{ $classe }}" name="motivo_contato">
         <option value="">Qual o motivo do contato?</option>
         <option value="1">Dúvida</option>
         <option value="2">Elogio</option>
         <option value="3">Reclamação</option>
     </select>
     <br>
-    <textarea namespace="mesagem" class="borda-preta">Preencha aqui a sua mensagem</textarea>
+    <textarea namespace="mesagem" class="{{ $classe }}">Preencha aqui a sua mensagem</textarea>
     <br>
-    <button type="submit" class="borda-preta">ENVIAR</button>
+    <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
