7fcba364afabd582a2ce89f4b2330b862da6bca5
Franccesco <franccesco_@hotmail.com>
2022-05-23 12:08:40 -0300

    Seção 7: Blade - Realizando include de views (@include)

--- resources/views/site/contact.blade.php
+++ resources/views/site/contact.blade.php
@@ -2,19 +2,6 @@
 
 @section('title', $title)
 @section('content')
-    <div class="topo">
-        <div class="logo">
-            <img src="{{ asset('img/logo.png') }}">
-        </div>
-
-        <div class="menu">
-            <ul>
-                <li><a href="{{ route('site.index') }}">Principal</a></li>
-                <li><a href="{{ route('site.about-us') }}">Sobre Nós</a></li>
-                <li><a href="{{ route('site.contact') }}">Contato</a></li>
-            </ul>
-        </div>
-    </div>
 
     <div class="conteudo-pagina">
         <div class="titulo-pagina">
